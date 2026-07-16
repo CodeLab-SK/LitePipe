ab2b1df9bcb31b2bb3b6a75b21a698e3ba496ffe
CodeLab-SK <sahilisthebest885@gmail.com>
2026-07-16 13:46:01 +0500

    misc: disable forced legacy tutorial dialog

--- app/src/main/java/com/hhst/youtubelite/ui/LinkTutorialDialog.java
+++ app/src/main/java/com/hhst/youtubelite/ui/LinkTutorialDialog.java
@@ -30,7 +30,7 @@ import java.util.List;
 
 public class LinkTutorialDialog {
 
-    private static final boolean FORCE_LEGACY_TEST = true;
+    private static final boolean FORCE_LEGACY_TEST = false;
 
     private final Context context;
     private AlertDialog dialog;
